3245807c5dd59993cc61740839e8adefeec8f239
karolbury98 <karolbury98@gmail.com>
2022-08-01 17:39:04 +0200

    edit, delete tasks

--- src/components/TodoApp.vue
+++ src/components/TodoApp.vue
@@ -2,7 +2,12 @@
   <div class="container">
     <h3 class="text-center mt-5">Lista zadań</h3>
     <div class="d-flex">
-      <input type="text" class="form-control" placeholder="Dodaj zadanie" v-model="enteredValue"/>
+      <input
+        type="text"
+        class="form-control"
+        placeholder="Dodaj zadanie"
+        v-model="enteredValue"
+      />
       <button @click="addTask" class="btn btn-warning">Dodaj</button>
     </div>
 
@@ -12,20 +17,23 @@
           <th scope="col">Zadanie</th>
           <th scope="col">Status</th>
           <th scope="col" class="text-center">#</th>
-          <th scope="col" class="text-center">#</th>
         </tr>
       </thead>
       <tbody>
         <tr v-for="(task, index) in tasks" :key="index">
-          <th>{{ task.name }}</th>
-          <td>{{ task.status }}</td>
-          <td>
-            <div class="text-center">
-              <span class="fa fa-pen-to-square"></span>
-            </div>
+          <th>
+            <span :class="{ finished: task.status === 'Zakończony' }">{{
+              task.name
+            }}</span>
+          </th>
+          <td style="width: 200px">
+            <span class="pointer" @click="changeStatus(index)">{{
+              task.status
+            }}</span>
           </td>
           <td>
-            <div class="text-center">
+            <!-- delete task -->
+            <div class="text-center" @click="deleteTask(index)">
               <span class="fa fa-trash-can"></span>
             </div>
           </td>
@@ -40,7 +48,8 @@ export default {
   name: "TodoApp",
   data() {
     return {
-        enteredValue: '',
+      enteredValue: "",
+      taskStatuses: ["Do wykonania", "W trakcie", "Zakończony"],
       tasks: [
         { name: "test1", status: "Do wykonania" },
         { name: "test2", status: "Do wykonania" },
@@ -49,11 +58,33 @@ export default {
   },
   methods: {
     addTask() {
-        this.tasks.push({
-            name: this.enteredValue,
-            status: 'Do wykonania'
-        });
-    }
+      this.tasks.push({
+        name: this.enteredValue,
+        status: "Do wykonania",
+      });
+
+      this.enteredValue = "";
+    },
+
+    deleteTask(index) {
+      this.tasks.splice(index, 1);
+    },
+
+    changeStatus(index) {
+      let newIndex = this.taskStatuses.indexOf(this.tasks[index].status);
+      if (++newIndex > 2) newIndex = 0;
+      this.tasks[index].status = this.taskStatuses[newIndex];
+    },
   },
 };
 </script>
+
+<style scoped>
+.pointer {
+  cursor: pointer;
+}
+
+.finished {
+  text-decoration: line-through;
+}
+</style>
